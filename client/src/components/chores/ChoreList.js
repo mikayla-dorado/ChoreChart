e9ff7c8e87a5e024b159afe639c53a3c6b97963b
mikayla dorado <mikayla.dorado17@gmail.com>
2024-02-07 16:21:35 -0600

    error with room search

--- client/src/components/chores/ChoreList.js
+++ client/src/components/chores/ChoreList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { getChores, deleteChore } from "../../managers/choreManager";
-import { Button, Card, Row, Col } from "reactstrap";
+import { Button, Card, Row, Col, Input } from "reactstrap";
 import "./Chore.css";
 
 export const ChoreList = ({ loggedInUser }) => {
     const [chores, setChores] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchByRoom, setSearchByRoom] = useState("");
+
+
     const navigate = useNavigate();
 
     const getAndSetChores = () => {
@@ -47,62 +51,104 @@ export const ChoreList = ({ loggedInUser }) => {
         }
     };
 
+    //this works
+    const filteredChores = chores.filter((chore) => {
 
-    return (
-        <div className="chore-list">
-            <h2 className="chores">Chores</h2>
-            {loggedInUser.roles.includes("Admin") && (
-                <Button color="secondary" onClick={handleCreateChoreBtn}>
-                    Create A New Chore
-                </Button>
-            )}
-            <Row xs="1" sm="2" md="3" lg="4">
-                {chores.map((c) => {
-                    // Declare isUserAssociated here
-                    const isUserAssociated = c.userChores.some((userChore) => userChore.userProfileId === loggedInUser.id);
+        const includesSearchTerm =
+            !searchTerm ||
+            (chore?.name || '').toLowerCase().includes(searchTerm.toLowerCase()) ||
+            (chore?.description || '').toLowerCase().includes(searchTerm.toLowerCase());
 
-                    return (
-                        <Col key={c.id} className="mb-4">
-                            <Card>
-                                <div className="d-flex flex-column p-3">
-                                    <h5>{c?.name}</h5>
-                                    <p>{c?.description}</p>
-                                    <Link to={`${c.id}`} className="btn btn-secondary">
-                                        Details
-                                    </Link>
-                                    {isUserAssociated && (
-                                        <Button
-                                            color="secondary"
-                                            onClick={(event) => handleCommentBtn(event, c.id)}
-                                            className="mb-2"
-                                        >
-                                            Add A Comment
-                                        </Button>
-                                    )}
-                                    {isUserAssociated && (
-                                        <>
+        return includesSearchTerm
+    });
+
+    //the search by room does not work yet
+    // const filteredUserChores = chores.userChore.filter((userChore) => {
+
+    //     const includesRoom =
+    //     !searchByRoom ||
+    //     // (userChore.room?.name || '').toLowerCase().includes(searchByRoom.toLowerCase());
+
+        
+    //     chores.userChores.some((userChore) => {
+    //         const roomName = userChore?.room?.name || '';
+    //         return roomName.toLowerCase().includes(searchByRoom.toLowerCase());
+    //     });
+
+    //      return includesRoom
+    // });
+
+    
+
+        return (
+            <div className="chore-list">
+                <h2 className="chores">Chores</h2>
+                {loggedInUser.roles.includes("Admin") && (
+                    <Button color="secondary" onClick={handleCreateChoreBtn}>
+                        Create A New Chore
+                    </Button>
+                )}
+                <input
+                    type="text"
+                    placeholder="Search Chores"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+
+                <input
+                    type="text"
+                    placeholder="Search By Room"
+                    value={searchByRoom}
+                    onChange={(e) => setSearchByRoom(e.target.value)}
+                />
+
+                <Row xs="1" sm="2" md="3" lg="4">
+                    {filteredChores.map((c) => {
+                        // Declare isUserAssociated here
+                        const isUserAssociated = c.userChores.some((userChore) => userChore.userProfileId === loggedInUser.id);
+
+                        return (
+                            <Col key={c.id} className="mb-4">
+                                <Card>
+                                    <div className="d-flex flex-column p-3">
+                                        <h5>{c?.name}</h5>
+                                        <p>{c?.description}</p>
+                                        <Link to={`${c.id}`} className="btn btn-secondary">
+                                            Details
+                                        </Link>
+                                        {isUserAssociated && (
                                             <Button
                                                 color="secondary"
-                                                onClick={(event) => handleDeleteBtn(event, c.id)}
+                                                onClick={(event) => handleCommentBtn(event, c.id)}
                                                 className="mb-2"
                                             >
-                                                Delete
+                                                Add A Comment
                                             </Button>
-                                            <Button
-                                                color="secondary"
-                                                onClick={(event) => handleEditBtn(event, c.id)}
-                                                className="mb-2"
-                                            >
-                                                Edit
-                                            </Button>
-                                        </>
-                                    )}
-                                </div>
-                            </Card>
-                        </Col>
-                    );
-                })}
-            </Row>
-        </div>
-    );
-};
+                                        )}
+                                        {isUserAssociated && (
+                                            <>
+                                                <Button
+                                                    color="secondary"
+                                                    onClick={(event) => handleDeleteBtn(event, c.id)}
+                                                    className="mb-2"
+                                                >
+                                                    Delete
+                                                </Button>
+                                                <Button
+                                                    color="secondary"
+                                                    onClick={(event) => handleEditBtn(event, c.id)}
+                                                    className="mb-2"
+                                                >
+                                                    Edit
+                                                </Button>
+                                            </>
+                                        )}
+                                    </div>
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </div>
+        );
+    };
